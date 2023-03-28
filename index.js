394f3cdbb62f8d4b32e30fc4036c9d8c001cec8b
MOX UFO <mox.ufo@gmail.com>
2023-03-28 10:47:47 -0400

    added functions to indexjs

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdownFile = require("./generateMarkdown");
 
-
+function writeToFile(data) {
+  fs.writeFile(`README.md`, generateMarkdownFile(data), (error) => {
+    error ? console.error(error) : console.log("worked!");
+  });
+};
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -11,7 +15,13 @@ const questions = [
   ];
 
 // // TODO: Create a function to initialize app
-
+function init() {
+    inquirer
+  .prompt(questions)
+  .then((response) => {
+    writeToFile(response);
+  });
+}
 
 // Function call to initialize app
 init();
