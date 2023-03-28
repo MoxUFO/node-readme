416be3ba520525e4b8550d30d9043d13c54ea5ec
MOX UFO <mox.ufo@gmail.com>
2023-03-28 10:48:49 -0400

    added question to array

--- index.js
+++ index.js
@@ -11,7 +11,52 @@ function writeToFile(data) {
 
 // TODO: Create an array of questions for user input
 const questions = [
-  
+    {
+      type: "input",
+      message: "What is Github name?",
+      name: "githubname",
+    },
+    {
+      type: "input",
+      message: "What is your Email address?",
+      name: "emailaddress",
+    },
+    {
+      type: "input",
+      message: "What is your Project name?",
+      name: "projectname",
+    },
+    {
+      type: "input",
+      message: "What is the description of your porject?",
+      name: "description",
+    },
+    {
+      type: "list",
+      message: "What license does your project include?",
+      name: "license",
+      choices: ["MIT",'Apache2.0','GPL'],
+    },
+    {
+      type: "input",
+      message: "What command should i run to install dependencies?",
+      name: "dependencies",
+    },
+    {
+      type: "input",
+      message: "What command should i run to run test?",
+      name: "test",
+    },
+    {
+      type: "input",
+      message: "What does the user need to know about using the repo?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What does the user need o know about contributing to the repo?",
+      name: "contribute",
+    },
   ];
 
 // // TODO: Create a function to initialize app
