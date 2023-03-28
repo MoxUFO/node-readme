9460d9a95282ba7606f6022b6ded4f29c652417d
MOX UFO <mox.ufo@gmail.com>
2023-03-28 10:44:15 -0400

    starter code and conneting the js modules to index js

---
+++ generateMarkdown.js
@@ -0,0 +1,8 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+
+// TODO: Create a function to generate markdown for README
+
+
+module.exports = generateMarkdownFile;
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdownFile = require("./generateMarkdown");
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  
+  ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to initialize app
 
-// TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
